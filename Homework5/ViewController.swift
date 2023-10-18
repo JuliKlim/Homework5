2d87aa6a5006f6793ca02ccf005e0f0d11698f65
Julija Klimenkova <146852581+JuliKlim@users.noreply.github.com>
2023-10-18 22:14:08 +0300

    fixed mistakes

--- Homework5/ViewController.swift
+++ Homework5/ViewController.swift
@@ -16,17 +16,14 @@ class ViewController: UIViewController {
         
         //      1. Посчитать количество символов в массиве.
         print("1.Количество символов в массиве:")
-        let result1 = array
-            .map{element in element.count}.reduce(0,+)
+        let result1 = array.map{element in element.count}.reduce(0,+)
         print(result1)
-        
         //      2. Если в строке символов меньше чем 5, то не использовать далее
         print("2.Строки массива с элементами > 5 :")
         let result2 = array.filter{element in
-            if element.count > 5 {return true}
-            else {return false}}
+            if element.count > 5 {true}
+            else {false}}
         print(result2)
-        
         //      3. Количество строк, где символов больше чем 10
         print("3.Количество строк, где символов больше чем 10:")
         let result3 = array.filter { element in
@@ -35,22 +32,25 @@ class ViewController: UIViewController {
         //      4.Распечатать строку в которой больше всего символов
         print("4.Строка, в которой больше всего символов:")
         if let max = array.max(by: {$1.count > $0.count}){print(max)}
-        
         //      5.Отсортировать массив, по возрастанию количества символов в строке
         print("5.Массив по возрастанию:")
-        let sortedArray = result2.sorted(){$0.count > $1.count}
+        let sortedArray = result2.sorted(){$0.count < $1.count}
         print(sortedArray)
-        
         //      6. Выбросить пробелы из строк в массиве
         print("6.Пробелы из строк удалены:")
-        let result6 = sortedArray.description.replacingOccurrences(of: " ", with: "")
-        print(result6)
-        
-       
-        // Do any additional setup after loading the view.
-        
-        
-    
+        for string in sortedArray{
+            let result6 = string.description.replacingOccurrences(of: " ", with: "")
+            print(result6)
+        }
         
     }
+    
+    
+    
+    // Do any additional setup after loading the view.
+    
+    
+    
+    
+    
 }
